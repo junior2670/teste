7e47dd21a8901bf8d257c00aed0cb4a638781c41
koffi kouame <koffikouameothiniel4@gmail.com>
2024-03-30 17:27:41 +0100

    initialisation du depot

---
+++ script.js
@@ -0,0 +1,77 @@
+//alert("hello");
+//alert("prenez place")
+// pour faire un commentaire 
+/*  pour faire plusieur commentaire */ 
+//les vrariable , les booleen , les float , les type primitif
+//let nom="koffi"//initalisation
+//nom="jean"// si la variable est deja declarer on ne la declare plus avec let .c'est l'affectation 
+//let  age;
+//let adesenfant =true
+//let anneedenaiissance=1990
+//let anneeactu=2024
+//alert(nom)
+//age=anneeactu%anneedenaiissance
+//alert(age) 
+//alert("the strange new world is back ")
+//les type complex 
+//let listN= ["code ","koffi"]//liste 
+//+concatenation
+//nan undefined//not a number variable d'erreur
+//alert(listN)
+//let b=true&&true;
+// let c =true //commparaison  <
+//comparaison === strict
+//comparaison de caractaire 
+//|| ou
+// principe de la selection 
+//let age=20;
+//if (age>=18){
+ //   alert("vous etes majeur")
+//} 
+//else{
+   // alert("vous etes mineur ")
+
+//function calaAge (dateN,dateactu){
+///    let a =dateactu-dateN    return a;
+//}
+//let age=calaAge(1964,2024)
+//alert(age)
+//if (age>=10){
+//alert("vous etes majeur ")
+//let n =0
+//while (n<100){let n =0
+//console.log("ok" +n)
+////n=n+1
+//}
+
+//}
+//else{
+ //   alert("vous etes mineur ")
+//}
+////for (let i =0;i<100){
+ ////   console.log("ko"+i)
+//}
+// exo declaration
+
+//for (let i=0;i<3 ;i=i+1){
+ //   let t=i+1
+  //  console.log("le nombre d'element" ,t)
+//}
+//let listem=[7,5,4,3]
+//let a=0
+//for(let j=0; j<=3;j=j+1){
+   // a=a+listem[j] 
+//}
+//let b=a/4
+//console.log(a)
+//console.log(b)
+let Listt=[5,8,7,9]
+let j=0  
+let a=0
+while(j<=3){
+    a=a+listt[j] 
+    j=j+1
+}
+let b=a/4
+console.log(a)
+console.log(b)
